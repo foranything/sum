2fa4ceb4f0e0a20f95c1db50cdcd0ea6c97d83e3
inhwan <ihlee@jlk-inspection.com>
2018-12-14 16:18:12 +0900

    더하기

--- HelloCSharpWF/Form1.Designer.cs
+++ HelloCSharpWF/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sum1 = new System.Windows.Forms.TextBox();
+            this.sum2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sumResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "여기를 클릭하세요";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sum1
+            // 
+            this.sum1.Location = new System.Drawing.Point(17, 60);
+            this.sum1.Name = "sum1";
+            this.sum1.Size = new System.Drawing.Size(90, 21);
+            this.sum1.TabIndex = 1;
+            this.sum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sum2
+            // 
+            this.sum2.Location = new System.Drawing.Point(153, 60);
+            this.sum2.Name = "sum2";
+            this.sum2.Size = new System.Drawing.Size(74, 21);
+            this.sum2.TabIndex = 2;
+            this.sum2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sumResult
+            // 
+            this.sumResult.Location = new System.Drawing.Point(300, 60);
+            this.sumResult.Name = "sumResult";
+            this.sumResult.Size = new System.Drawing.Size(56, 21);
+            this.sumResult.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label2.Location = new System.Drawing.Point(123, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(483, 243);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sumResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sum2);
+            this.Controls.Add(this.sum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sum1;
+        private System.Windows.Forms.TextBox sum2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sumResult;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- HelloCSharpWF/Form1.cs
+++ HelloCSharpWF/Form1.cs
@@ -16,5 +16,52 @@ namespace HelloCSharpWF
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            int num1 = 10;
+            int num2 = 2;
+            int sum = num1 + num2;
+            label1.Text = sum.ToString();
+        
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+
+            if (String.IsNullOrWhiteSpace(sum1.Text) || String.IsNullOrWhiteSpace(sum2.Text) || !int.TryParse(sum1.Text, out num1) || !int.TryParse(sum2.Text, out num2))
+            {
+                MessageBox.Show("숫자를 입력해 주세요");
+            }
+            else
+            {
+                sumResult.Text = Add(num1, num2).ToString();
+            }
+
+        }
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
     }
 }
